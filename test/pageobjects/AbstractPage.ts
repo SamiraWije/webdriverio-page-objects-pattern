028ce070ba6e184548121f059fe333b06443a15b
SamiraWije <sameerawijesekara8@gmail.com>
2025-04-25 12:23:20 +0530

    Add data helper functions

--- test/pageobjects/AbstractPage.ts
+++ test/pageobjects/AbstractPage.ts
@@ -2,4 +2,22 @@ export default class AbstractPage{
     public async waitForSeconds(seconds: number) {
         await browser.pause(seconds * 1000)
     }
+
+    //Driver Helpers - Create utility functions such as Set Browser Size and Network Speed
+    public async setFullHDResolution(){
+        await browser.setWindowSize(1920, 1080);
+    }
+
+    public async setNetworkSpeedTo3G() {
+        await browser.throttleNetwork('Regular3G');
+    }
+
+    public async takeScreenshot(path: string) {
+        await browser.saveScreenshot(path);
+    }
+
+    //Data Helpers - Generate Random Data
+    public async generateRandomNumber(){
+        return Math.floor(Math.random() * 1000000 + 1);
+    }
 }
